d2bdf518969caff491bc540b7e08dd86a88cb015
Tyler Erskine <tyler.w.erskine@gmail.com>
2015-02-22 12:47:14 -0500

    initial commit of working project

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
-# Gemfile
 source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'holidapi'
--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
-# app.rb
 require 'sinatra'
+require 'holidapi'
+require 'date'
 
 class MyWebApp < Sinatra::Base
   get '/' do
+	@time = Time.now
+    @holidays = HolidApi.get(country: 'us', year: @time.year, month: @time.month)
+    @birthHolidays = HolidApi.get(country: 'us', year: '1994', month: '7')
     erb :index
-  end
-  get '/about' do
-    erb :about
   end
 end
