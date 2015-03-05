d9c05141d4e5cc7de095b1494413e6b261abacfc
Tyler Erskine <tyler.w.erskine@gmail.com>
2015-03-05 15:03:49 -0500

    added required attributes and fixed a tabbed space

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require 'holidapi'
 require 'date'
 
 class MyWebApp < Sinatra::Base
-  get '/' do
-    if params != nil and params.any?
-	  begin
-        @holidays = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
-      rescue
-        @holidays = []
-      end
-    end
-    erb :index
-  end
+	get '/' do
+		if params != nil and params.any?
+			begin
+				@holidays = HolidApi.get(country: params['country'], year: params['year'], month: params['month'])
+			rescue
+				@holidays = []
+			end
+		end
+		erb :index
+	end
 end
